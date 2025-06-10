2ac0307cfdb9111bffdf4d01810c94a29c28d227
fidelMB <fidel.morales.briones@gmail.com>
2025-06-10 12:40:00 +0530

    created cxr class

--- data/datasets.py
+++ data/datasets.py
@@ -90,3 +90,47 @@ class MMStarDataset(Dataset):  # https://huggingface.co/datasets/Lin-Chen/MMStar
             "answer": answer
         }
     
+
+class CXRDataset(Dataset): # pending to upload to huggingface, but this class is already prepared to receive the dataset without problems
+    def __init__(self, dataset, tokenizer, image_processor):
+        self.dataset = dataset
+        self.tokenizer = tokenizer
+        self.image_processor = image_processor
+        
+    def __len__(self):
+        return len(self.dataset)
+    
+    def __getitem__(self, idx):
+        item = self.dataset[idx]
+        
+        image = item['image']
+            
+        # Now process the image
+        if isinstance(image, Image.Image):
+            if image.mode != 'RGB':
+                image = image.convert('RGB')
+            processed_image = self.image_processor(image)
+        else:
+            print(f"Error processing image at index {idx}")
+            # Create empty tensor with right dimensions as fallback
+            processed_image = torch.zeros(3, cfg.VLMConfig.vit_img_size, cfg.VLMConfig.vit_img_size)
+        
+        # Process text (also a list)
+        text_data = item['texts']
+        if isinstance(text_data, list) and len(text_data) > 0:
+            text = text_data[0]
+        else:
+            text = text_data
+
+        question = text['question']
+        # Add EOS token to the answer to train model to predict it, enabling correct stopping during generation
+        answer = text['answer'] + self.tokenizer.eos_token
+
+        formatted_text = f"Question: {question} Answer:"
+
+        return {
+            "image": processed_image,
+            "text_data": formatted_text,
+            "answer": answer
+        }
+    
